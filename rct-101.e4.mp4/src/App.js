1d80e7e5f7e48e68da4a7ff1e3c72a44a06c114d
Deepak Choudhary <97680719+Dgr81996@users.noreply.github.com>
2022-06-14 02:22:42 +0530

    Add files via upload

---
+++ rct-101.e4.mp4/src/App.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+    
+   
+    <input type={"submit"}value={"previous"} />
+    <input type={"submit"}value={"next"} />
+    <input type={"submit"}value={"sort"} />
+                
+               
+                    
+                
+               
+    </div>
+  );
+}
+
+export default App;
